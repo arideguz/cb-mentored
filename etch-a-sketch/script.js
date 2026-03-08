e37095cdbf29546727f1fc8e7fdae4b85cec5d65
Arianne de Guzman <ariannedeguzman@berkeley.edu>
2026-03-08 16:28:44 -0700

    black pen, erase, and clear buttons work

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -3,10 +3,10 @@ const totalSize = 650;
 const board = document.getElementById("board");
 board.style.width = totalSize;
 board.style.height = totalSize;
-const erase = document.getElementById("erase");
-const blackPen = document.getElementById("black-pen");
-const secret = document.getElementById("secret");
-const clear = document.getElementById("clear");
+const erase = document.querySelector("#erase");
+const blackPen = document.querySelector("#black-pen");
+const secret = document.querySelector("#secret");
+const clear = document.querySelector("#clear");
 
 
 for (let i = 0; i < size * size; i++) {
@@ -19,13 +19,41 @@ for (let i = 0; i < size * size; i++) {
     board.appendChild(piece);
 }
 
+// variable ting
+
+const defaultMode = "classic";
+let currentMode = defaultMode;
+
+function setMode(newMode) {
+    currentMode = newMode;
+    drawState();
+}
+
+erase.onclick = () => setMode("eraseMode");
+blackPen.onclick = () => setMode("classic");
+secret.onclick = () => setMode("secretMode");
+clear.onclick = () => setMode("clearMode");
+
 function drawState() {
-    blackPen.addEventListener("click", fillBlack);
-    erase.addEventListener("click", undo);
+    if (currentMode == "classic") {
+        fillBlack();
+    }
+    else if (currentMode == "eraseMode") {
+        undo();
+    }
+    else if (currentMode == "clearMode") {
+        const pieces = document.querySelectorAll(".piece");
+        pieces.forEach((piece) => {piece.style.background = "pink"});
+    }
+    else if (currentMode == "secretMode") {
+        
+    }
+    console.log(currentMode);
 }
 
 function fillBlack() {
     const pieces = document.querySelectorAll(".piece");
+    pieces.forEach((piece) => {piece.removeEventListener("mouseover", black)})
     pieces.forEach((piece) => {piece.addEventListener("mouseover", black)});
 }
 
@@ -34,11 +62,18 @@ function black() {
 }
 
 function erasePen() {
-    this.style.background = "white";
+    this.style.background = "pink";
 }
 function undo() {
     const pieces = document.querySelectorAll(".piece");
-    pieces.forEach((piece) => {piece.addEventListener("mouseover", erasePen)})
+    pieces.forEach((piece) => {piece.removeEventListener("mouseover", erasePen)})
+    pieces.forEach((piece) => {piece.addEventListener("mouseover", erasePen)});
+}
+
+function fillBlack() {
+    const pieces = document.querySelectorAll(".piece");
+    pieces.forEach((piece) => {piece.removeEventListener("mouseover", black)})
+    pieces.forEach((piece) => {piece.addEventListener("mouseover", black)});
 }
 
 drawState()
